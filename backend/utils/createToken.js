74d2ec699a66d48790bddb1f26800568191d25f9
Sachith77 <sachith.24bcs10403@sst.scaler.com>
2025-10-26 01:28:36 +0530

    Enhance JWT cookie settings: update secure and sameSite attributes based on production environment

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -6,10 +6,12 @@ const generateToken = (res, userId) => {
   });
 
   // Set JWT as an HTTP-Only Cookie
+  const isProduction = process.env.NODE_ENV === "production";
+
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: process.env.NODE_ENV === "development" ? "lax" : "strict",
+    secure: isProduction,
+    sameSite: isProduction ? "strict" : "lax",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 
